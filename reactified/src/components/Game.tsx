ffbf604302765190def9bb1276a0f7af06e97aac
Collin Choy <collin@choy.me>
2020-03-15 23:20:38 -0400

    Add global stylesheet & some basic game components.

--- reactified/src/components/Game.tsx
+++ reactified/src/components/Game.tsx
@@ -1,18 +1,45 @@
 import React from "react";
 import styled from "styled-components";
-import { simpleLevelPlan } from "reactified/src/levels.js";
+import { simpleLevelPlan } from "reactified/src/levels";
 
-const Level = styled.div`
+const LEVEL_SIZE = "500px";
+
+const Level = styled.table`
   background: rgb(52, 166, 251);
-  width: 500px;
-  height: 500px;
+  width: ${LEVEL_SIZE};
+  height: ${LEVEL_SIZE};
 `;
 
-const Game = () => {
-  // const renderLevel = ({plan}) => {
-  //   plan.
-  // }
-  return <Level>{simpleLevelPlan}</Level>;
+const Row = styled.tr`
+  display: flex;
+  width: 100%;
+`;
+
+const StyledTile = styled.td`
+  width: 20px;
+  height: 20px;
+`;
+
+const Tile: React.FC<{ type: string }> = ({ type }) => {
+  return <StyledTile>{type}</StyledTile>;
+};
+
+const Game: React.FC<{ plan?: string }> = ({ plan = simpleLevelPlan }) => {
+  const renderLevel = () => {
+    const lines = plan.split("\n");
+    return (
+      <tbody>
+        {lines.map((line: string, lineNo) => (
+          <Row key={lineNo}>
+            {line.split("").map((character, colNo) => (
+              <Tile key={colNo} type={character} />
+            ))}
+          </Row>
+        ))}
+      </tbody>
+    );
+  };
+  return <Level>{renderLevel()}</Level>;
 };
 
 export default Game;
